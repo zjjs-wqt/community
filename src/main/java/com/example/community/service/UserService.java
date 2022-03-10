57e57e2e8a76eaea35652e243035866ef31da712
wangqintao <419384048@qq.com>
2022-03-10 21:17:23 +0800

    2022/3/10 v2

--- src/main/java/com/example/community/service/UserService.java
+++ src/main/java/com/example/community/service/UserService.java
@@ -27,6 +27,7 @@ public class UserService implements CommunityConstant {
     @Autowired
     private  LoginTicketMapper loginTicketMapper;
     @Autowired
+
     private MailClient mailClient;//邮件客户端
 
     @Autowired
@@ -121,6 +122,38 @@ public class UserService implements CommunityConstant {
             map.put("usernameMsg","账号不能为空!");
             return map;
         }
+        if(StringUtils.isBlank(password)){
+            map.put("passwordMsg","密码不能为空!");
+            return map;
+        }
+
+        //验证账号
+        User user = userMapper.selectByName(username);
+        if(user == null ){
+            map.put("usernameMsg","该账号不存在!");
+            return map ;
+        }
+
+        //验证状态
+        if( user.getStatus() == 0 ){
+            map.put("usernameMsg","该账号未激活!");
+            return map;
+        }
+
+        //验证密码
+        password = CommunityUtil.md5(password+user.getSalt());
+
+        if(!user.getPassword().equals(password)){
+            map.put("passwordMsg","密码不正确");
+            return map;
+        }
+
+        //生成登录凭证
+        LoginTicket loginTicket = new LoginTicket();
+        loginTicket.setUserId(user.getId());
+        loginTicket.setTicket(CommunityUtil.generateUUID());
+        loginTicket.setStatus(0);
+        loginTicket.setExpired(new Date(System.currentTimeMillis() + expiredSeconds * 1000 ));
 
 
         return map;
